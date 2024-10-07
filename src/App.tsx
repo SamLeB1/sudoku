3908f74845a2fba93f592e939badae16b4fdd323
Sam <leblanc.SAL1@gmail.com>
2024-10-07 09:55:22 -0300

    styled input buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Sudoku from "./components/Sudoku/Sudoku.tsx";
 import InputButtons from "./components/InputButtons/InputButtons.tsx";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <div className="App">
       <Sudoku />
       <InputButtons />
-    </>
+    </div>
   );
 }
--- src/components/InputButtons/InputButtons.tsx
+++ src/components/InputButtons/InputButtons.tsx
@@ -14,75 +14,87 @@ export default function InputButtons() {
 
   return (
     <div className="input-buttons">
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(1)}
-      >
-        1
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(2)}
-      >
-        2
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(3)}
-      >
-        3
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(4)}
-      >
-        4
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(5)}
-      >
-        5
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(6)}
-      >
-        6
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(7)}
-      >
-        7
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(8)}
-      >
-        8
-      </button>
-      <button
-        className="btn-number"
-        type="button"
-        onClick={() => handleClick(9)}
-      >
-        9
-      </button>
-      <button
-        className="btn-erase"
-        type="button"
-        onClick={() => handleClick(0)}
-      >
-        Erase
+      <div className="action-buttons">
+        <div className="btn-action">
+          <button type="button" />
+          <p>Undo</p>
+        </div>
+        <div className="btn-action">
+          <button type="button" onClick={() => handleClick(0)} />
+          <p>Erase</p>
+        </div>
+        <div className="btn-action">
+          <button type="button" />
+          <p>Hint</p>
+        </div>
+      </div>
+      <div className="number-buttons">
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(1)}
+        >
+          1
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(2)}
+        >
+          2
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(3)}
+        >
+          3
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(4)}
+        >
+          4
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(5)}
+        >
+          5
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(6)}
+        >
+          6
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(7)}
+        >
+          7
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(8)}
+        >
+          8
+        </button>
+        <button
+          className="btn-number"
+          type="button"
+          onClick={() => handleClick(9)}
+        >
+          9
+        </button>
+      </div>
+      <button className="btn-new-game" type="button">
+        New Game
       </button>
     </div>
   );
